900c0c5a5c92d8df60e10045a5686b14365b5c24
Stefan Roggensack <someone561@gmx.de>
2014-01-03 17:26:57 +0100

    remove Capitalisation from script name

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 `EpisodeCheck` is a simple Script which check if in a folder of tv series one is missing. It uses the filename parser from [tvnamer](https://github.com/dbr/tvnamer). This script is very basic. Currently it only search for the highest episode number in the current directory and display all missing episodes as list.
 
 # Usage
-    python EpisodeCheck.py <path>
+    python episodecheck.py <path>
--- episodechecktest.py
+++ episodechecktest.py
@@ -1,20 +1,20 @@
 import unittest
-import EpisodeCheck
+import episodecheck
 
 
 class Test(unittest.TestCase):
 
 
     def testFileNameParse(self):
-        p1 = EpisodeCheck.Finder(["ABC S03E1.avi"]);
+        p1 = episodecheck.Finder(["ABC S03E1.avi"]);
         self.assertEqual(p1.Missing, []);
-        p2 = EpisodeCheck.Finder(["ABC S03E01.avi"]);
+        p2 = episodecheck.Finder(["ABC S03E01.avi"]);
         self.assertEqual(p2.Missing, []);
-        p3 = EpisodeCheck.Finder(["ABC S03E01-02.avi"]);
+        p3 = episodecheck.Finder(["ABC S03E01-02.avi"]);
         self.assertEqual(p3.Missing, []);        
-        p4 = EpisodeCheck.Finder(["ABC S03E01-02.avi", "ABC S03E05.avi"]);
+        p4 = episodecheck.Finder(["ABC S03E01-02.avi", "ABC S03E05.avi"]);
         self.assertEqual(p4.Missing, [3,4]);
-        p5 = EpisodeCheck.Finder(["ABC S03E10.avi"]);
+        p5 = episodecheck.Finder(["ABC S03E10.avi"]);
         self.assertEqual(p5.Missing, [1,2,3,4,5,6,7,8,9]);
 
 if __name__ == "__main__":
